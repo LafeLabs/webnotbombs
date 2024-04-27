07beef4b98f0f341bee6a118982f04e9a5a6a900
Trash Robot <lafelabs@gmail.com>
2024-04-27 09:29:05 -0600

    quantumart web

--- squares/trashbook/replicator.php
+++ squares/trashbook/replicator.php
@@ -1,6 +1,6 @@
     <?php
-    
-    $dnaurl = "https://raw.githubusercontent.com/LafeLabs/webnotbombs/main/trashbook/data/dna.txt";
+
+    $dnaurl = "https://raw.githubusercontent.com/LafeLabs/trashbook/main/data/dna.txt";
     
     if(isset($_GET["dna"])){
         $dnaurl = $_GET["dna"];
@@ -17,7 +17,7 @@
     mkdir("iconsymbols");
     
 
-    copy("https://raw.githubusercontent.com/LafeLabs/webnotbombs/main/trashbook/php/replicator.txt","replicator.php");
+    copy("https://raw.githubusercontent.com/LafeLabs/trashbook/main/php/replicator.txt","replicator.php");
     
     
     
